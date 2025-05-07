d2b555596bd5ae430a28368503fc3f9d10ab4e54
Alkerg <albert.arg500@gmail.com>
2025-05-06 22:55:00 -0500

    feat: add project files for activities 8,9,10,11,12

---
+++ Actividad8/proyecto-actividad-8/tests/test_stock.py
@@ -0,0 +1,17 @@
+# tests/test_stock.py
+import pytest
+from src.carrito import Carrito, Producto
+
+def test_agregar_producto_excede_stock():
+    """
+    Red: Se espera que al intentar agregar una cantidad mayor a la disponible en stock se lance un ValueError.
+    """
+    # Arrange
+    # Suponemos que el producto tiene 5 unidades en stock.
+    producto = Producto("ProductoStock", 100.00)
+    producto.stock = 1
+    carrito = Carrito()
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+        carrito.agregar_producto(producto, cantidad=6)
